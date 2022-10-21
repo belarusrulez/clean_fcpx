d6fab673a49d398fa5956b3149adf729213f8aa8
belarusrulez <ivan@fedarau.com>
2022-10-20 19:22:13 -0700

    init commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# sh script that cleans and zips your final cut pro x project. (fcpx)
+
+ 
+
+## Installation
+
+copy this code and execute in directory where you want to save your script
+```bash
+
+```
+
+from script directory run
+## Usage
+```bash
+./clean_fcpx.sh ${FULL_PATH_TO_YOUR_LIBRARY}
+```
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ clean_fcpx.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "need library directory path"
+    exit 1
+fi
+cd "$1"
+dir_name=$(basename $(pwd))
+echo "$(pwd)"
+array=( $(find . -type f -name 'CurrentVersion.fcpevent'  -maxdepth 2 -mindepth 2 | sed -r 's|/[^/]+$||' |sort |uniq) )
+echo "$array"
+# loop over it
+# shellcheck disable=SC2068
+for i in ${array[@]}
+do
+    echo "rm -rf $1/$i/Render\ Files"
+    rm -rf $1/$i/Render\ Files
+    echo "rm -rf $1/$i/Shared\ Items"
+    rm -rf $1/$i/Shared\ Items
+    echo "rm -rf $1/$i/Transcoded\ Media"
+    rm -rf $1/$i/Transcoded\ Media
+    rm -rf $1/.fcpcache
+done
+cd ..
+echo "zip  --symlinks -r "$dir_name".zip "$dir_name""
+zip --symlinks -r "$dir_name".zip "$dir_name"
